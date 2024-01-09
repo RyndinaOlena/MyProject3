36c97bb85879aeca6f464eb76a72eb9a5fd86ab9
Ryndina Olena <ryndinaolena@gmail.com>
2024-01-09 17:42:41 +0200

    navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Content/Home/Home";
+import Portfolio from "./Content/Portfolio/Portfolio";
+import Pages from "./Content/Pages/Pages";
+import Services from "./Content/Services/Services";
+import Footer from "./Content/Footer/Footer";
+import Contact from "./Content/Contact/Contact";
+import Navbar from "./Content/Home/Navbar";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/portfolio' element={<Portfolio />} />
+        <Route exact path='/pages' element={<Pages />} />
+        <Route exact path='/services' element={<Services />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/footer' element={<Footer />} />
+      </Routes>
+    </>
   );
 };
---
+++ src/components/Content/Home/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { RxCross1 } from "react-icons/rx";
+import { PiCouchThin } from "react-icons/pi";
+import { Link, NavLink } from 'react-router-dom';
+import css from './Navbar.module.css'
+
+const Navbar = () => {
+    const [isModalOpen, setModal] = useState(false)
+    const openModal = () => {
+        setModal(true)
+    }
+    const closeModal = () => {
+        setModal(false)
+    }
+    return (
+        <div className={css.boxHeader}>
+            <div className={css.navBar} >
+                <PiCouchThin className={css.logo} style={{ color: 'white' }} size={50} />
+                <button className={css.btnOpenMenu} onClick={openModal}><p className={css.textMenu}>Menu</p></button>
+            </div>
+            {isModalOpen && (<div className={css.modalOverlay} >
+                <div className={css.modal}>
+                    <button className={css.closeModal} onClick={closeModal}><RxCross1 size={20} style={{ color: 'white' }} /></button>
+                    <div className={css.navMenuPosition}>
+                        <NavLink className={css.navMenu} style={{ textDecoration: 'none' }}>
+                            <Link className={css.menuLink} onClick={closeModal} to='/'>Home</Link>
+                            <Link className={css.menuLink} onClick={closeModal} to='portfolio'>Portfolio</Link>
+                            <Link className={css.menuLink} onClick={closeModal} to='pages'>Pages</Link>
+                            <Link className={css.menuLink} onClick={closeModal} to='services'>Services</Link>
+                            <Link className={css.menuLink} onClick={closeModal} to='contact'>Contact</Link>
+                        </NavLink></div>
+
+
+                </div>
+            </div >)
+            }
+
+        </div>
+    )
+}
+
+export default Navbar
